232a7358adb7604ff3eee2d42eee1b6048a329d6
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-05-23 23:49:34 +0200

    first commit

--- src/main/java/za/ac/cput/Entity/Employee.java
+++ src/main/java/za/ac/cput/Entity/Employee.java
@@ -1,12 +1,14 @@
+
+
 package za.ac.cput.Entity;
 
 public class Employee {
-    private  String employeeNumber, firstName, lastname;
+    private String employeeNumber, firstName, lastName;
 
     private Employee(Builder builder) {
-        this.employeeNumber= builder.employeeNumber;
-        this.firstName= builder.firstName;
-        this.lastname= builder.lastname;
+        this.employeeNumber = builder.employeeNumber;
+        this.firstName = builder.firstName;;
+        this.lastName = builder.lastName;
     }
 
     @Override
@@ -14,41 +16,37 @@ public class Employee {
         return "Employee{" +
                 "employeeNumber='" + employeeNumber + '\'' +
                 ", firstName='" + firstName + '\'' +
-                ", lastname='" + lastname + '\'' +
+                ", lastName='" + lastName + '\'' +
                 '}';
     }
 
-    public  static class   Builder{
-        private  String employeeNumber, firstName, lastname;
+    public static class Builder {
+        private String employeeNumber, firstName, lastName;
 
         public Builder setEmployeeNumber(String employeeNumber) {
             this.employeeNumber = employeeNumber;
-
-            return  this;
+            return this;
         }
 
         public Builder setFirstName(String firstName) {
             this.firstName = firstName;
-            return  this;
-        }
-
-        public Builder setLastname(String lastname) {
-            this.lastname = lastname;
             return this;
         }
-        public Employee build(){
 
-            return new Employee(this);
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
         }
 
-       public  Builder copy(Employee employee) {
-            this.employeeNumber=employee.employeeNumber;
-            this.firstName=employee.firstName;
-            this.lastname=employee.lastname;
+        public Builder copy(Employee employee) {
+            this.employeeNumber = employee.employeeNumber;
+            this.firstName = employee.firstName;
+            this.lastName = employee.lastName;
+            return this;
+        }
 
-             return  this;
-       }
-
+        public Employee build() {
+            return new Employee(this);
+        }
     }
-
 }
