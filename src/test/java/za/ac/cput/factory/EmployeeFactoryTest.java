c6608aba02fea4ad8fb3a52315248002a7d53264
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-06-08 18:25:25 +0200

    saving test sets

--- src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -5,14 +5,50 @@ import org.junit.jupiter.api.Test;
 import za.ac.cput.entity.user.Employee;
 import za.ac.cput.factory.user.EmployeeFactory;
 
-class EmployeeFactoryTest {
-    @Test
-      public void   createEmployee(){
-        Employee employee= EmployeeFactory.createEmployee("Zookz", "Zeehar");
-                Assert.assertEquals(String.valueOf(employee), employee.getFirstName()
-                        ,employee.getFirstName());
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+class EmployeeFactoryTest {
+    Employee employee1;
+    Employee employee2;
+    Employee employee3;
+
+
+    @Test
+    public void createEmployee() {
+        employee1 = EmployeeFactory.createEmployee("Zookz", "Zookel");
+        employee2 = employee1;
+        employee3 = EmployeeFactory.createEmployee("Zookz", "Zookel");
+
+        System.out.println(" " + employee1);
+        System.out.println(" " + employee2);
+        System.out.println(" " + employee3);
+
+        Assert.assertSame("Passed", employee1, employee2);
+
+        Assert.assertEquals("Passed", employee1, employee2);
+
+        Assert.assertNotEquals("Passed", employee1, employee3);
+
+
+        assertTimeout(Duration.ofSeconds(5), () -> delaySecond()); // pass
+    }
+
+    @Test
+    void delaySecond() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
+
 }
+
+
+
+
